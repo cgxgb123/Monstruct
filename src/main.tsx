65f5560c0ed4594382424c1cfc329af85bc34dda
CGB <christian.blunt12@gmail.com>
2026-02-10 14:49:49 -0500

    chore: setup Apollo client with auth middleware

--- src/main.tsx
+++ src/main.tsx
@@ -10,6 +10,27 @@ import {
 import './index.css';
 import App from './App.tsx';
 
+const httpLink = new HttpLink({
+  uri: 'http://localhost:3001/graphql',
+});
+
+const authLink = new ApolloLink((operation, forward) => {
+  const token = localStorage.getItem('id_token');
+
+  operation.setContext({
+    headers: {
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  });
+
+  return forward(operation);
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ApolloProvider client={client}>
