1cc0abbd6ae334e2635a3e1119311403a77702ee
CGB <christian.blunt12@gmail.com>
2026-02-10 14:47:47 -0500

    feat: import dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
+import { ApolloProvider } from '@apollo/client/react';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import {
+  ApolloClient,
+  InMemoryCache,
+  HttpLink,
+  ApolloLink,
+} from '@apollo/client';
+import './index.css';
+import App from './App.tsx';
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </StrictMode>,
 );
