55c9857a97002b9d0bd42eaaae99133cb0a585b2
CGB <christian.blunt12@gmail.com>
2026-02-16 12:28:14 -0500

    feat(utils): add typing icon urls

---
+++ src/utils/teraTypes.ts
@@ -0,0 +1,33 @@
+export const TERA_TYPES = {
+  Normal:
+    'https://f2.toyhou.se/file/f2-toyhou-se/images/57770983_08ThED9oIdXkPim.png',
+  Fire: 'https://f2.toyhou.se/file/f2-toyhou-se/thumbnails/57771337_CPd.png',
+  Water:
+    'https://f2.toyhou.se/file/f2-toyhou-se/images/57771446_Ze0tSn7CuywQ0QQ.png',
+  Electric:
+    'https://f2.toyhou.se/file/f2-toyhou-se/images/57771492_F9IFBgpAxY6RT90.png',
+  Grass:
+    'https://f2.toyhou.se/file/f2-toyhou-se/images/57771544_J7mKS45eI0mTxcz.png',
+  Ice: 'https://f2.toyhou.se/file/f2-toyhou-se/images/57771582_NcKTNnZ3Q9VkwpG.png',
+  Fighting:
+    'https://f2.toyhou.se/file/f2-toyhou-se/images/57771678_vbMdpogdcalRfRw.png',
+  Poison:
+    'https://f2.toyhou.se/file/f2-toyhou-se/images/57771764_lq1Sznw34si9Bcv.png',
+  Ground:
+    'https://f2.toyhou.se/file/f2-toyhou-se/images/57771868_iJsqA1t4I9hkNjc.png',
+  Flying:
+    'https://f2.toyhou.se/file/f2-toyhou-se/images/57771935_g8GqjIlGqDnIpDo.png',
+  Psychic:
+    'https://f2.toyhou.se/file/f2-toyhou-se/images/57771970_ess4yja9A2YxubU.png',
+  Bug: 'https://f2.toyhou.se/file/f2-toyhou-se/images/57772029_92YX2B025hf1hbj.png',
+  Rock: 'https://f2.toyhou.se/file/f2-toyhou-se/images/57772128_Vw3gF6VwIhUvaQb.png',
+  Ghost:
+    'https://f2.toyhou.se/file/f2-toyhou-se/images/57772253_6QhzGMFmhoZQsJo.png',
+  Dragon:
+    'https://f2.toyhou.se/file/f2-toyhou-se/images/57772368_oylsjbrKep6cNqU.png',
+  Dark: 'https://f2.toyhou.se/file/f2-toyhou-se/thumbnails/57772459_Q3R.png',
+  Steel:
+    'https://f2.toyhou.se/file/f2-toyhou-se/images/57772528_AHavbzP6CsvJIRn.png',
+  Fairy:
+    'https://f2.toyhou.se/file/f2-toyhou-se/images/57772673_ZDF27uNKfuPTIs3.png',
+};
