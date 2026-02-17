9decbf4cee03ca70d496664ab0d80e366eab14bd
CGB <christian.blunt12@gmail.com>
2026-02-17 01:48:05 -0500

    tera types

--- src/utils/teraTypes.ts
+++ src/utils/teraTypes.ts
@@ -1,33 +1,39 @@
-export const TERA_TYPES = {
-  Normal:
-    'https://f2.toyhou.se/file/f2-toyhou-se/images/57770983_08ThED9oIdXkPim.png',
-  Fire: 'https://f2.toyhou.se/file/f2-toyhou-se/thumbnails/57771337_CPd.png',
-  Water:
-    'https://f2.toyhou.se/file/f2-toyhou-se/images/57771446_Ze0tSn7CuywQ0QQ.png',
-  Electric:
-    'https://f2.toyhou.se/file/f2-toyhou-se/images/57771492_F9IFBgpAxY6RT90.png',
-  Grass:
-    'https://f2.toyhou.se/file/f2-toyhou-se/images/57771544_J7mKS45eI0mTxcz.png',
-  Ice: 'https://f2.toyhou.se/file/f2-toyhou-se/images/57771582_NcKTNnZ3Q9VkwpG.png',
-  Fighting:
-    'https://f2.toyhou.se/file/f2-toyhou-se/images/57771678_vbMdpogdcalRfRw.png',
-  Poison:
-    'https://f2.toyhou.se/file/f2-toyhou-se/images/57771764_lq1Sznw34si9Bcv.png',
-  Ground:
-    'https://f2.toyhou.se/file/f2-toyhou-se/images/57771868_iJsqA1t4I9hkNjc.png',
-  Flying:
-    'https://f2.toyhou.se/file/f2-toyhou-se/images/57771935_g8GqjIlGqDnIpDo.png',
-  Psychic:
-    'https://f2.toyhou.se/file/f2-toyhou-se/images/57771970_ess4yja9A2YxubU.png',
-  Bug: 'https://f2.toyhou.se/file/f2-toyhou-se/images/57772029_92YX2B025hf1hbj.png',
-  Rock: 'https://f2.toyhou.se/file/f2-toyhou-se/images/57772128_Vw3gF6VwIhUvaQb.png',
-  Ghost:
-    'https://f2.toyhou.se/file/f2-toyhou-se/images/57772253_6QhzGMFmhoZQsJo.png',
-  Dragon:
-    'https://f2.toyhou.se/file/f2-toyhou-se/images/57772368_oylsjbrKep6cNqU.png',
-  Dark: 'https://f2.toyhou.se/file/f2-toyhou-se/thumbnails/57772459_Q3R.png',
-  Steel:
-    'https://f2.toyhou.se/file/f2-toyhou-se/images/57772528_AHavbzP6CsvJIRn.png',
-  Fairy:
-    'https://f2.toyhou.se/file/f2-toyhou-se/images/57772673_ZDF27uNKfuPTIs3.png',
+import Bug from '../assets/gem/BUG.png';
+import Dark from '../assets/gem/DARK.png';
+import Dragon from '../assets/gem/Dragon.png';
+import Electric from '../assets/gem/Electric.png';
+import Fairy from '../assets/gem/Fairy.png';
+import Fighting from '../assets/gem/Fighting.png';
+import Fire from '../assets/gem/Fire.png';
+import Flying from '../assets/gem/Flying.png';
+import Ghost from '../assets/gem/Ghost.png';
+import Grass from '../assets/gem/Grass.png';
+import Ground from '../assets/gem/Ground.png';
+import Ice from '../assets/gem/Ice.png';
+import Normal from '../assets/gem/Normal.png';
+import Poison from '../assets/gem/Poison.png';
+import Psychic from '../assets/gem/Psychic.png';
+import Rock from '../assets/gem/Rock.png';
+import Steel from '../assets/gem/Steel.png';
+import Water from '../assets/gem/Water.png';
+
+export const TERA_TYPES: Record<string, string> = {
+  Bug,
+  Dark,
+  Dragon,
+  Electric,
+  Fairy,
+  Fighting,
+  Fire,
+  Flying,
+  Ghost,
+  Grass,
+  Ground,
+  Ice,
+  Normal,
+  Poison,
+  Psychic,
+  Rock,
+  Steel,
+  Water,
 };
