bed97981407f09d2c86c800cafb9783bfc07143b
CGB <christian.blunt12@gmail.com>
2026-02-17 20:25:16 -0500

    feat: add tera and type icon asset mapping

--- src/utils/teraTypes.ts
+++ src/utils/teraTypes.ts
@@ -1,3 +1,4 @@
+// src/utils/teraTypes.ts:
 import Bug from '../assets/gem/BUG.png';
 import Dark from '../assets/gem/DARK.png';
 import Dragon from '../assets/gem/Dragon.png';
@@ -36,4 +37,38 @@ export const TERA_TYPES: Record<string, string> = {
   Rock,
   Steel,
   Water,
+};
+
+export const TYPE_ICONS: Record<string, string> = {
+  normal:
+    'https://f2.toyhou.se/file/f2-toyhou-se/images/57770983_08ThED9oIdXkPim.png',
+  fire: 'https://f2.toyhou.se/file/f2-toyhou-se/images/57771337_CPdMoer424Z6qOl.png',
+  water:
+    'https://f2.toyhou.se/file/f2-toyhou-se/images/57771446_Ze0tSn7CuywQ0QQ.png',
+  electric:
+    'https://f2.toyhou.se/file/f2-toyhou-se/images/57771492_F9IFBgpAxY6RT90.png',
+  grass:
+    'https://f2.toyhou.se/file/f2-toyhou-se/images/57771544_J7mKS45eI0mTxcz.png',
+  ice: 'https://f2.toyhou.se/file/f2-toyhou-se/images/57771582_NcKTNnZ3Q9VkwpG.png',
+  fighting:
+    'https://f2.toyhou.se/file/f2-toyhou-se/images/57771678_vbMdpogdcalRfRw.png',
+  poison:
+    'https://f2.toyhou.se/file/f2-toyhou-se/images/57771764_lq1Sznw34si9Bcv.png',
+  ground:
+    'https://f2.toyhou.se/file/f2-toyhou-se/images/57771868_iJsqA1t4I9hkNjc.png',
+  flying:
+    'https://f2.toyhou.se/file/f2-toyhou-se/images/57771935_g8GqjIlGqDnIpDo.png',
+  psychic:
+    'https://f2.toyhou.se/file/f2-toyhou-se/images/57771970_ess4yja9A2YxubU.png',
+  bug: 'https://f2.toyhou.se/file/f2-toyhou-se/images/57772029_92YX2B025hf1hbj.png',
+  rock: 'https://f2.toyhou.se/file/f2-toyhou-se/images/57772128_Vw3gF6VwIhUvaQb.png',
+  ghost:
+    'https://f2.toyhou.se/file/f2-toyhou-se/images/57772253_6QhzGMFmhoZQsJo.png',
+  dragon:
+    'https://f2.toyhou.se/file/f2-toyhou-se/images/57772368_oylsjbrKep6cNqU.png',
+  dark: 'https://f2.toyhou.se/file/f2-toyhou-se/images/57772459_Q3RqXpbb5uwGN0V.png',
+  steel:
+    'https://f2.toyhou.se/file/f2-toyhou-se/images/57772528_AHavbzP6CsvJIRn.png',
+  fairy:
+    'https://f2.toyhou.se/file/f2-toyhou-se/images/57772673_ZDF27uNKfuPTIs3.png',
 };
